edbc8b21b0403ad978caa48878d583ebd8f8f0a5
Vinicius Fillos <vinifillos@gmail.com>
2024-05-16 22:41:40 -0300

    feat: mscloudgateway implementado

---
+++ microservices/mscloudgateway/src/main/java/io/github/vinifillos/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,27 @@
+package io.github.vinifillos.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MscloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator builder(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+				.build();
+	}
+}
